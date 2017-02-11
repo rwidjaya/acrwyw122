f85268d5ad3c02aa81c78c5a1aa330777be7c432
Yuxi Wu <yuxiwu@uchicago.edu>
2017-02-10 21:49:29 -0600

    deleted some whitespace because ocd

--- allsides.py
+++ allsides.py
@@ -8,7 +8,6 @@ import util
 
 asurl = "http://www.allsides.com/bias/bias-ratings?field_news_source_type_tid=2&field_news_bias_nid=1&field_featured_bias_rating_value=1&title="
 
-
 def source_info(soup):
     odd = soup.find_all("tr", class_="odd")
     odd = [(o, o.find_next("div", class_="rate-details")) for o in odd]
@@ -39,7 +38,6 @@ def source_info(soup):
 
     return info
 
-
 def go():
     soup = util.get_soup(asurl)
     info = source_info(soup)
@@ -55,7 +53,6 @@ def go():
             row = [key]
             row += [val for val in info[key]]
             writer.writerow(row)
-
 
 if __name__ == "__main__":
     filename = "as.csv"
