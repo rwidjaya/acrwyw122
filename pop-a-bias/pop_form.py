ca484487e15f4088d075f51702146615a6a31fa8
Cynette791 <reginalwidjaya@gmail.com>
2017-03-15 13:29:47 -0500

    Remove pop-a-bias

--- pop-a-bias/pop_form.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField
-from wtforms.validators import DataRequired
-
-class UrlForm(FlaskForm):
-    '''
-
-    '''
-    url = StringField('url', validators=[DataRequired])
